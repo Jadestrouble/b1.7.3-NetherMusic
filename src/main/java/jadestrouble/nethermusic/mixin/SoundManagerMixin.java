cf43baca360a5c9f7ea704fea9242cbad4af9903
Jade <Jadestrouble@gmail.com>
2024-04-28 13:23:39 -0700

    Updated to new Mappings and StAPI

--- src/main/java/jadestrouble/nethermusic/mixin/SoundManagerMixin.java
+++ src/main/java/jadestrouble/nethermusic/mixin/SoundManagerMixin.java
@@ -1,11 +1,10 @@
 package jadestrouble.nethermusic.mixin;
 
 import jadestrouble.nethermusic.sounds.NetherMusicSoundManager;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.options.GameOptions;
-import net.minecraft.client.sound.SoundEntry;
-import net.minecraft.client.sound.SoundHelper;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.sound.Sound;
+import net.minecraft.client.sound.SoundManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,32 +14,33 @@ import paulscode.sound.SoundSystem;
 
 import static jadestrouble.nethermusic.sounds.NetherMusicSoundManager.*;
 
-@Mixin(SoundHelper.class)
-public class SoundHelperMixin {
-    @Shadow private static boolean initialized;
+@Mixin(SoundManager.class)
+public class SoundManagerMixin {
+    @Shadow private static boolean started;
     @Shadow private GameOptions gameOptions;
     @Shadow private static SoundSystem soundSystem;
-    @Shadow private int musicCountdown;
+    @Shadow private int timeUntilNextSong;
 
-    @Inject(method = "setLibsAndCodecs", at = @At("TAIL"))
+    @Inject(method = "start", at = @At("TAIL"))
     private void netherMusic_setLibsAndCodecs(CallbackInfo info) {
         NetherMusicSoundManager.init(gameOptions, soundSystem);
     }
-    @Inject(method = "handleBackgroundMusic", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void netherMusic_handleBackgroundMusic(CallbackInfo info) {
-        if (!initialized) return;
-        @SuppressWarnings("deprecated")
-        Minecraft minecraft = (Minecraft) FabricLoader.getInstance().getGameInstance();
-        boolean isNether = minecraft != null && minecraft.level != null && minecraft.level.dimension.id == -1;
+        if (!started) return;
+
+        Minecraft minecraft = NetherMusicSoundManager.getMinecraft();
+        boolean isNether = minecraft != null && minecraft.world != null && minecraft.world.dimension.id == -1;
         NetherMusicSoundManager.setInTheNether(isNether);
         if (isNether) {
-            if (gameOptions.music == 0.0f) return;
+            if (gameOptions.musicVolume == 0.0f) return;
             if (soundSystem.playing(MUSIC_KEY) || soundSystem.playing(STREAMING_KEY)) return;
-            if (musicCountdown > 0) return;
-            SoundEntry soundEntry = NetherMusicSoundManager.getRandomMusic(RANDOM);
-            musicCountdown = 12000 + RANDOM.nextInt(12000);
-            soundSystem.backgroundMusic(MUSIC_KEY, soundEntry.soundUrl, soundEntry.soundName, false);
-            soundSystem.setVolume(MUSIC_KEY, gameOptions.music * 0.25F);
+            if (timeUntilNextSong > 0) return;
+            NetherMusicSoundManager.LOGGER.info("Attempting to play song!");
+            Sound sound = NetherMusicSoundManager.getRandomMusic(RANDOM);
+            timeUntilNextSong = 12000 + RANDOM.nextInt(12000);
+            soundSystem.backgroundMusic(MUSIC_KEY, sound.soundFile, sound.id, false);
+            soundSystem.setVolume(MUSIC_KEY, gameOptions.musicVolume * 0.25F);
             soundSystem.play(MUSIC_KEY);
             info.cancel();
         }
--- src/main/java/jadestrouble/nethermusic/sounds/NetherMusicSoundManager.java
+++ src/main/java/jadestrouble/nethermusic/sounds/NetherMusicSoundManager.java
@@ -2,10 +2,12 @@ package jadestrouble.nethermusic.sounds;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.options.GameOptions;
-import net.minecraft.client.sound.SoundEntry;
-import net.modificationstation.stationapi.api.registry.Identifier;
-import net.modificationstation.stationapi.api.registry.ModID;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.sound.Sound;
+import net.modificationstation.stationapi.api.util.Identifier;
+import net.modificationstation.stationapi.api.util.Namespace;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import paulscode.sound.SoundSystem;
@@ -14,7 +16,6 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-
 @Environment(EnvType.CLIENT)
 public class NetherMusicSoundManager {
     public static final String STREAMING_KEY = "streaming";
@@ -23,16 +24,22 @@ public class NetherMusicSoundManager {
     private static boolean inTheNether = false;
     private static GameOptions gameOptions;
     private static SoundSystem soundSystem;
-    public static final ModID MOD_ID = ModID.of("nethermusic");
+    public static final Namespace NAMESPACE = Namespace.of("nethermusic");
     public static final Logger LOGGER = LogManager.getLogger();
     public static Identifier id(String name) {
-        return MOD_ID.id(name);
+        return NAMESPACE.id(name);
     }
     public static URL getURL(String path) {
         return Thread.currentThread().getContextClassLoader().getResource(path);
     }
+    @SuppressWarnings("deprecation")
+    public static Minecraft getMinecraft() {
+        return (Minecraft) FabricLoader.getInstance().getGameInstance();
+    }
+
     public static void setInTheNether(boolean inTheNether) {
         if (NetherMusicSoundManager.inTheNether != inTheNether) {
+            NetherMusicSoundManager.LOGGER.info("Switched Dimension!");
             soundSystem.stop(MUSIC_KEY);
         }
         NetherMusicSoundManager.inTheNether = inTheNether;
@@ -42,25 +49,27 @@ public class NetherMusicSoundManager {
         NetherMusicSoundManager.soundSystem = soundSystem;
     }
     public static boolean updateMusicVolume() {
-        if (!inTheNether || gameOptions.music == 0.0f) return false;
-        soundSystem.setVolume(MUSIC_KEY, gameOptions.music * 0.25F);
+        NetherMusicSoundManager.LOGGER.info("Changed Music Volume!");
+        if (!inTheNether || gameOptions.musicVolume == 0.0f) return false;
+        soundSystem.setVolume(MUSIC_KEY, gameOptions.musicVolume * 0.25F);
         return true;
     }
-    private static final Map<Identifier, SoundEntry> SOUNDS = new HashMap<>();
-    public static SoundEntry getSound(Identifier id) {
+    private static final Map<Identifier, Sound> SOUNDS = new HashMap<>();
+    public static Sound getSound(Identifier id) {
+        NetherMusicSoundManager.LOGGER.info("Tried to get sounds!");
         if (id == null) return null;
         return SOUNDS.computeIfAbsent(id, key -> {
-            String path = "assets/" + key.modID + "/stationapi/music/" + key.id + ".ogg";
+            String path = "assets/" + key.namespace + "/stationapi/music/" + key.path + ".ogg";
             URL url = NetherMusicSoundManager.getURL(path);
             if (url == null) {
                 NetherMusicSoundManager.LOGGER.warn("Sound " + path + " is missing!");
                 return null;
             }
-            return new SoundEntry(path, url);
+            return new Sound(path, url);
         });
     }
 
-    private static final SoundEntry[] MUSIC = new SoundEntry[] {
+    private static final Sound[] MUSIC = new Sound[] {
             getSound(NetherMusicSoundManager.id("nether1")),
             getSound(NetherMusicSoundManager.id("nether2")),
             getSound(NetherMusicSoundManager.id("nether3")),
@@ -77,7 +86,8 @@ public class NetherMusicSoundManager {
         shuffleMusic(new Random());
     }
 
-    public static SoundEntry getRandomMusic(Random random) {
+    public static Sound getRandomMusic(Random random) {
+        if (MUSIC.length == 1) return MUSIC[MUSIC_INDEX_DATA[0]];
         if (musicIndex == MUSIC.length) {
             byte value = MUSIC_INDEX_DATA[musicIndex - 1];
             shuffleMusic(random);
@@ -97,5 +107,3 @@ public class NetherMusicSoundManager {
         }
     }
 }
-
-
